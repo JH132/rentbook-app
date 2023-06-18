f7369feb15e52156b2f565cf4a35205098b7b1cf
safirmagentaa <safirmagenta22@student.ub.ac.id>
2023-06-18 19:58:12 +0700

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('auth');
 
+Route::get('/login', [AuthController::class, 'login'])->name('login');
