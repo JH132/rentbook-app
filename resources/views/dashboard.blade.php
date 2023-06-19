bb35ce5ce126ad8fbf2de18ec7ae29e6598aa638
safirmagentaa <safirmagenta22@student.ub.ac.id>
2023-06-19 12:38:30 +0700

    initial commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,10 @@
-@extends('layouts.mainlayout')
-
-@section('content')
-    <h1>Welcome to Dashboard</h1>
-    <!-- Add your dashboard content here -->
-@endsection
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+</head>
+<body>
+    <h1>Welcome to the Dashboard</h1>
+    <p>You are logged in as {{ session('username') }}</p>
+</body>
+</html>
