97ad816522335fd0a1d8865298a036193ca7f113
JH132 <ruridesfianti@gmail.com>
2023-06-19 12:48:15 +0700

    initial commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,27 +8,5 @@ use Illuminate\Http\RedirectResponse;
 
 class AuthController extends Controller
 {
-    public function showLoginForm()
-    {
-        return view('login');
-    }
-
-    // public function login(Request $request)
-    // {
-    //     $credentials = $request->only('username', 'password');
-    //     if ($credentials['username'] === 'magenta' && $credentials['password'] === 'rahasia') {
-    //         return redirect('/dashboard');
-    //     } else {
-    //         return redirect()->back()->withErrors('Username atau password salah.');}
-    // }
-
-    public function login(Request $request)
-    {
-    $credentials = $request->only('username', 'password');
-    if ($credentials['username'] === 'magenta' && $credentials['password'] === 'rahasia') {
-        return redirect()->route('dashboard');
-    } else {
-        return redirect()->back()->withErrors('Username atau password salah.');
-    }
-    }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -8,15 +8,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
-
-
-// Route::middleware(['auth'])->group(function () {
-//     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-// });
-
 Route::get('/anggota/create', [AnggotaController::class, 'create'])->name('anggota.create');
 Route::post('/anggota', [AnggotaController::class, 'store'])->name('anggota.store');
 Route::get('/anggota', [AnggotaController::class, 'index'])->name('anggota.index');
