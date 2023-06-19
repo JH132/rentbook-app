3e81f868ee7e944ebe1c43aff298c6ef944d3279
JH132 <ruridesfianti@gmail.com>
2023-06-19 12:46:28 +0700

    initial commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Http\RedirectResponse;
+
 class AuthController extends Controller
 {
- 
+    public function showLoginForm()
+    {
+        return view('login');
+    }
+
+    // public function login(Request $request)
+    // {
+    //     $credentials = $request->only('username', 'password');
+    //     if ($credentials['username'] === 'magenta' && $credentials['password'] === 'rahasia') {
+    //         return redirect('/dashboard');
+    //     } else {
+    //         return redirect()->back()->withErrors('Username atau password salah.');}
+    // }
+
+    public function login(Request $request)
+    {
+    $credentials = $request->only('username', 'password');
+    if ($credentials['username'] === 'magenta' && $credentials['password'] === 'rahasia') {
+        return redirect()->route('dashboard');
+    } else {
+        return redirect()->back()->withErrors('Username atau password salah.');
+    }
+    }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,71 @@
-@extends('layouts.mainlayout')
-
-@section('content')
-    <h1>Welcome to Dashboard</h1>
-    <!-- Add your dashboard content here -->
-@endsection
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Dashboard</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      background-color: #f8f9fa; /* Warna latar belakang */
+    }
+    
+    .dashboard {
+      text-align: center;
+    }
+    
+    .dashboard button {
+      display: inline-block;
+      margin: 10px;
+      padding: 20px;
+      font-size: 18px;
+      color: #fff;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+      background-image: linear-gradient(to right, #667eea, #764ba2); /* Warna button */
+      box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.3);
+      transition: transform 0.3s ease-in-out;
+      width: 200px;
+    }
+    
+    .dashboard button:hover {
+      transform: scale(1.05);
+    }
+    
+    /* Menerapkan kontras warna latar belakang dengan button */
+    .dashboard.light-bg button {
+      background-image: linear-gradient(to right, #764ba2, #667eea);
+    }
+  </style>
+</head>
+<body>
+  <div class="dashboard">
+    <button class="btn btn-primary anggota" onclick="openAnggotaPage()">Anggota</button>
+    <button class="btn btn-secondary buku" onclick="openBukuPage()">Buku</button>
+    <button class="btn btn-success peminjaman" onclick="openPeminjamanPage()">Peminjaman</button>
+  </div>
+  
+  <script>
+    function openAnggotaPage() {
+      alert("Halaman Anggota");
+      // Tambahkan logika atau aksi yang ingin dilakukan saat button Anggota ditekan
+    }
+    
+    function openBukuPage() {
+      alert("Halaman Buku");
+      // Tambahkan logika atau aksi yang ingin dilakukan saat button Buku ditekan
+    }
+    
+    function openPeminjamanPage() {
+      alert("Halaman Peminjaman");
+      // Tambahkan logika atau aksi yang ingin dilakukan saat button Peminjaman ditekan
+    }
+  </script>
+  
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+</html>
