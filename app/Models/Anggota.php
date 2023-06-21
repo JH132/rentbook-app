c2962fe4203784694dd5a80e6d8fc25b96e43d8e
Shania <shaniaedina@gmail.com>
2023-06-21 16:54:39 +0700

    initial commit

--- app/Models/Anggota.php
+++ app/Models/Anggota.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Anggota extends Model
 {
     use HasFactory;
+    protected $table = 'anggota';
+    protected $primaryKey = 'id_anggota';
+    public $timestamps = false;
+
+    protected $fillable = ['Nama', 'Alamat', 'Email', 'Nomor_Telepon', 'Tanggal_Bergabung'];
+
+    public function buku()
+    {
+        return $this->belongsTo(Buku::class, 'id_buku');
+    }
+
+    public function anggota()
+    {
+        return $this->belongsTo(Anggota::class, 'id_anggota');
+    }
+
+    public function peminjamans(){
+    return $this->hasMany(Peminjaman::class, 'id_anggota');
+    }
+
 }
